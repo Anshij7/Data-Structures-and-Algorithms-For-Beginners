ca01ac1b934bee05fb3a677713243cfd1f2e7d5d
Subham <97989643+Subham-Maity@users.noreply.github.com>
2022-10-23 19:55:31 +0530

    Linked list add page 1

--- 01. Array Data Structures/1. All Codes in The Notes/2. Insertion/CodeXam.java
+++ 01. Array Data Structures/1. All Codes in The Notes/2. Insertion/CodeXam.java
@@ -21,3 +21,27 @@ class CodeXam {
     }
 
 }
+
+
+
+//Same - revision
+
+import java.util.Arrays;
+class CodeXam{
+    public static void main(String[] args) {
+        int [] arr = {45,25,26,75};
+        int pos = 2;
+        int element = 69;
+
+        int[] newArr = new int[arr.length+1];
+        for (int i = 0 ; i < pos ; i++){
+            newArr[i] = arr[i];
+        }
+        newArr[pos] = element;
+        for (int i = pos; i <arr.length ; i++){
+            newArr[i+1] = arr[i];
+        }
+        System.out.println(Arrays.toString(newArr));
+
+    }
+}
